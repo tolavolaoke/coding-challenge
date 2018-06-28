166e0644c7bf44870cc7fc32108db2b21843464f
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-06-28 17:50:55 +0100

    fixed the error assigned all six squares different colours

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 
 // All the HTML elements in the form of variables
+window.onload = function() {
+   
 
 var numSquares = 6; //stores the number of squares that will be available in the game as per mode
-var colors = []; //an empty array which contains six or three RGB colour code generated every time the game is reset or the mode is changed
 var pickedColor; //the colour/option block selected by the user on every click
 var squares = document.querySelectorAll(".square"); //an array of all the squares on the page that are available as options. No of elements depend on the mode of the game.
 var resetButton = document.querySelector("#reset"); //the new game button
-var modeButtons = document.querySelectorAll(".mode");//an array that has the easy and hard mode buttons in.
+var modeButtons = document.querySelectorAll(".mode"); //an array that has the easy and hard mode buttons in.
 
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(255, 225, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 255, 0)"
+]; //an array which contains six or three RGB colour code generated every time the game is reset or the mode is changed
+
+
+for (i=0; i<squares.length; i++) {
+squares[i].style.backgroundColor = colors[i]
+}
+
+
+
+};
+
+for(i=0; i<= squares.length; i++) {
+    squares[i].addeventListeners('click', function() {
+      alert('option was clicked');
+    });
+  }
