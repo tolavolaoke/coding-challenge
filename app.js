9098e0a987407ea64704abaf27f529f1b2bf5aec
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-07-10 17:18:53 +0100

    completed carousel, started slider example

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
+//Carousel
 
+//contains all the images in an array
+var image = ['a', 'b', 'c', 'd', 'e'];
+var i = 0;
 
-var responsiveSlider = function() {
+// function for next slide
+function nextImage() {
 
-var slider = document.getElementById("slider");
-var sliderWidth = slider.offsetWidth;
-var sliderList = document.getElementById("sliderWrap");
-var count = 1;
-var items = sliderList.querySelectorAll("li").length;
-var prev = document.getElementById("prev");
-var prev = document.getElementById("next");
+  if (i < image.length - 1) {
+    i = i + 1;
+  } else {
+    i = 0;
+  }
+  document.getElementById('carousel').innerHTML = "<img src='assets/" + image[i] + ".png'>";
+}
 
-window.addEventListener(resize )
+// function for previous slide
+function prevImage() {
 
+  if (i < image.length && i > 0) {
+    i = i - 1;
+  } else {
+    i = image.length - 1;
+  }
+  document.getElementById('carousel').innerHTML = "<img src='assets/" + image[i] + ".png'>";
 }
 
 
-window.onload = function() {
-   responsiveSlider();
+//Slider
 
- }
+var block = [];
+var slide = 0;
+
+function nextSlide(){
+  for(var slide = 0; slide < array.length; slide++){
+    array.length[block]
+    document.getElementById("slider")
+}
+}
