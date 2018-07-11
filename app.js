fe7b08c754bf9be0f205d677f6dbc572af05e81a
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-07-11 17:19:45 +0100

    completed slider thats automates

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 //contains all the images in an array
 var image = ['a', 'b', 'c', 'd', 'e'];
-var i = 0;
+var i = 0; //counter
+var carousel;
 
 // function for next slide
 function nextImage() {
@@ -27,14 +28,42 @@ function prevImage() {
 }
 
 
+
+
+
+
 //Slider
 
-var block = [];
-var slide = 0;
+window.onload = function () {
+  var slider = document.getElementById("slider");
+  var colours = ['red', 'orange', 'yellow', 'green', 'blue'];
+  var counter = 0; //counts which poisition we are in, in the array, starts at 0 (red)
 
-function nextSlide(){
-  for(var slide = 0; slide < array.length; slide++){
-    array.length[block]
-    document.getElementById("slider")
-}
+
+  function colourChange() {
+
+      //to reset the counter back to 0, 
+      //if the counter is greater than or equal to the total number of colours in array
+      //the array starts at 0 again
+    if (counter >= colours.length){
+      counter = 0;
+    }
+
+
+    slider.style.background = colours[counter]; //changes the colour of the slider
+    console.log(colours[counter]);
+    counter++; //adds 1 to the counter
+  
+
+  }
+//myTimer variable
+//assigned to start a timer, colours change every second, for the autosrcoll
+
+  var myTimer = setInterval(colourChange, 1000); 
+
+  slider.onclick =  function(){
+
+    clearInterval(myTimer); // stops the timer when the slider is clicked
+    slider.innerHTML = "Timer stopped"; //Inserts the text Timer stoped onto the carosoul
+  };
 }
